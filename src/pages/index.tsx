f4c41a7f900b3c219135e6969af3c900e6193e7d
jindun619 <jindun619@naver.com>
2025-04-22 01:43:13 +0800

    feat: implement CRUD (excluding U) REST API for tracks, introduce TrackDTO, and create admin page (in progress)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from "next";
 import RankingList from "@/components/RankList";
-import { RankedTrack } from "@/types/RankedTrack";
+import { RankedTrack } from "@/types/rankedTrack";
 import { convertRawTrackToTrack } from "@/utils/convert";
 import { rankTracksByScore } from "@/utils/rankTracksByScore";
 import { getTracks } from "@/lib/redis/getTracks";
