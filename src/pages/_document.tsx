d50528a6a557e9f52120eb99a367686cee1e7383
jindun619 <jindun619@naver.com>
2025-05-13 11:56:51 +0800

    site icon added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en" data-theme="emerald">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/asset/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/asset/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/asset/favicon-16x16.png"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
